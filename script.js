e54e7215b5e4131a1e850c293c3d5a89ecc56b43
Turlito5 <artursoufelipe@gmail.com>
2025-10-14 17:03:11 -0300

    Letreco

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-  const idLinha = 1; 
+ var idLinha = 1; 
  const Linha = document.getElementById("linha" + idLinha); 
  const button = document.getElementById("button");
- const Input = document.getElementById("entrada"); 
+ const Input = document.getElementById("entrada");
+ var Certa = "artur" 
  button.addEventListener("click", testarPalavra);
- Input.addEventListener("keydonw", enter);
+ Input.addEventListener("keydown", enter);
 
  function enter(Tecla) {
 
@@ -11,18 +12,48 @@
         testarPalavra()
 
     }
-
-
-
  }
-
-
-
-
  function testarPalavra() {
 
-    let palavra = document.getElementById("entrada").value; 
+    let palavra = document.getElementById("entrada").value;
+    Certa = Certa.toUpperCase();
+    palavra = palavra.toUpperCase();
+    
+    for( let i=0; i < palavra.length; i++ ) {
+      if (palavra[i] === Certa[i]) {
+         criarElemento(palavra[i])
+      }
+      if(palavra === Certa || palavra[i] != Certa[i] ) {
 
+         criarElemento2(palavra[i]);
+         console.log(palavra);
 
+      } 
+    }
+    idLinha++;
+    Linha = document.getElementById("linha" + idLinha); 
 
  }
+
+ function criarElemento(letra) {
+   console.log(letra)
+   let item = document.createElement("div");
+   item.classList.add("letra")
+   item.classList.add("certa");
+   item.textContent = letra
+   Linha.appendChild(item); 
+   
+ 
+
+ }
+  function criarElemento2(letra) {
+         console.log(letra)
+   let item = document.createElement("div");
+   item.classList.add("letra")
+   item.classList.add("Posicao");
+   item.textContent = letra
+   Linha.appendChild(item); 
+   
+
+
+   }
